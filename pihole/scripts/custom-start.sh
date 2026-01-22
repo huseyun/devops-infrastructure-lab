f70ecd7364b3a00d737664c779063030a917c371
Hüseyin Akaslan <hsynaksln@gmail.com>
2026-01-22 22:42:20 +0300

    last touches before merging

--- pihole/scripts/custom-start.sh
+++ pihole/scripts/custom-start.sh
@@ -5,21 +5,17 @@ echo "🔥 [CUSTOM-INIT] Ozel baslangic scripti calisiyor..."
 mkdir -p /etc/dnsmasq.d
 CONF_FILE="/etc/dnsmasq.d/99-tailscale.conf"
 
-# --- AYARLAR (Aynı kalıyor) ---
 echo "address=/.${LOCAL_DOMAIN}/${TARGET_IP}" > "$CONF_FILE"
 echo "server=/ts.net/100.100.100.100" >> "$CONF_FILE"
 echo "rev-server=100.64.0.0/10,100.100.100.100" >> "$CONF_FILE"
 echo "rev-server=fd7a:115c:a1e0::/48,100.100.100.100" >> "$CONF_FILE"
 
 if [ -f "$CONF_FILE" ]; then
-    echo "✅ [CUSTOM-INIT] Dosya olusturuldu."
+    echo "[CUSTOM-INIT] Dosya olusturuldu."
 else
-    echo "❌ [CUSTOM-INIT] Dosya olusturulamadi!"
+    echo "[CUSTOM-INIT] Dosya olusturulamadi!"
 fi
 
-echo "🚀 [CUSTOM-INIT] Gorev tamamlandi, Orjinal Baslangic (/init) tetikleniyor..."
+echo "[CUSTOM-INIT] Gorev tamamlandi, Orjinal Baslangic (/init) tetikleniyor..."
 
-# --- DÜZELTME BURADA ---
-# pihole-FTL'i değil, konteynerin orjinal başlangıç sistemini (/init) çalıştırıyoruz.
-# Bu sistem gerekli izinleri ayarlar ve sonra FTL'i kendisi başlatır.
 start.sh
