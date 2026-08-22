0850366f37c353fbd31aae1e96bdb317aad5b93c
Hüseyin Akaslan <hsynaksln@gmail.com>
2026-08-22 21:14:44 +0300

    remove secret environment variables fix

--- terraform/env_devops.ps1
+++
@@ -1,4 +0,0 @@
-$env:PROXMOX_VE_ENDPOINT   = "https://192.168.1.131:8006/"
-$env:PROXMOX_VE_API_TOKEN  = "root@pam!terraform=800405c1-705a-48d1-8bec-b47871ad9b76"
-$env:PROXMOX_VE_USERNAME   = "root@pam"
-$env:PROXMOX_VE_INSECURE   = "true"
