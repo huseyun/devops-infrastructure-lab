b7b4f1baf3e73240ffcc480c895057319bb81a10
Hüseyin Akaslan <hsynaksln@gmail.com>
2026-08-30 21:34:36 +0300

    add last changes to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "guests" {
   description = "list of all guests. the value should point to a guest_specs value."
   type        = map(string)
   validation {
-    condition = alltrue([for rol in values(guests) : contains(keys(var.guest_specs), rol)])
+    condition = alltrue([for rol in values(var.guests) : contains(keys(var.guest_specs), rol)])
     error_message = "every guest should point to a defined guest spec."
   }
 }
